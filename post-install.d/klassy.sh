a35ac2a2e406863159647929bdf287f529f86f84
filippofg <filippo.faggion@pm.me>
2025-04-12 19:58:35 +0200

    Add KVM and new klassy (WIP) scripts for debian testing

--- post-install.d/klassy.sh
+++ post-install.d/klassy.sh
@@ -4,13 +4,17 @@ set -e
 set -u
 set -o pipefail
 
-DIR=post-install-klassy
-
-mkdir $DIR
-cd $DIR
-git clone https://github.com/paulmcauley/klassy.git 
-chmod +x klassy/install.sh
-./klassy/install.sh
-cd ..
-rm -rf $DIR
-
+#if [ ! -e /etc/debian-version ]; then
+#    echo "Not a Debian distro, skipping."
+#    exit
+#fi
+#
+#echo "Adding klassy repo..."
+#echo 'deb http://download.opensuse.org/repositories/home:/paul4us/Debian_Testing/ /' | sudo tee /etc/apt/sources.list.d/home:paul4us.list > /dev/null
+#curl -fsSL https://download.opensuse.org/repositories/home:paul4us/Debian_Testing/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_paul4us.gpg > /dev/null
+#
+#echo "Updating package index..."
+#sudo apt update > /dev/null
+#sudo apt install klassy -y
+#
+#echo "All done!"
---
+++ post-install.d/kvm.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+if [ ! -F /etc/debian_version ]; then
+    echo "Not a Debian distro, skipping."
+    exit 0
+fi
+
+echo "Updating package index..."
+sudo apt update
+
+echo "Installing virtualization packages..."
+sudo apt install -y \
+    software-properties-common \
+    qemu-system \
+    libvirt-daemon-system \
+    ovmf \
+    virt-manager
+
+echo "Enabling contrib and non-free repos..."
+sudo apt-add-repository contrib
+sudo apt-add-repository non-free
+
+# libvirt
+echo "Adding current user to libvirt group..."
+sudo adduser $(whoami) libvirt
+
+echo "Importing VM configuration..."
+VM_NAME=proto-type-00-tiny10
+THIS_FOLDER="$(dirname $0)"
+CONF_DIR="$THIS_FOLDER/../other-configs/usr/share"
+
+sudo cp qemu/* /usr/share/qemu
+
+VM_CONF="$VM_NAME.xml"
+cp "$CONF_DIR/$VM_CONF" .
+sed -i 's/\/usr\/share\/edk2\/x64\/OVMF_VARS.4m.fd/\/usr\/share\/OVMF\/OVMF_VARS_4M.fd/g' $VM_CONF
+sed -i 's/\/usr\/share\/edk2\/x64\/OVMF_CODE.4m.fd/\/usr\/share\/OVMF\/OVMF_CODE_4M.fd/g' $VM_CONF
+
+sudo virsh define --file $VM_CONF
+echo ""
+echo "REMEMBER: edit the VM with the appropriate devices for passthrough."
+echo ""
+echo "All done!"
+
